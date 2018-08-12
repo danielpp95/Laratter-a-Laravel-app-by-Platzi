ac9464251c22585776f516307a34526d2a3b7092
Daniel Pereira <danielpp95@gmail.com>
2018-08-12 09:49:24 -0400

    clase 16 - Autenticación de Usuarios

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ Route::get('/', 'PagesController@home');
 Route::get('/messages/{message}', 'MessagesController@show');
 
 Route::post('/messages/create', 'MessagesController@create');
+
+Auth::routes();
+
